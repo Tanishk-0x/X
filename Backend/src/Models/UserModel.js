a56a11751716db412bd37899bc63da8cc845de39
Tanishk-0x <tanishknamdev981@gmail.com>
2025-10-29 13:46:38 +0530

    Creating Model

---
+++ Backend/src/Models/UserModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose') ; 
+
+const UserSchema = new mongoose.Schema({
+
+    name : {
+        type : String , 
+        required : true 
+    }, 
+    email : {
+        type : String , 
+        required : true 
+    }, 
+    password : {
+        type : String ,
+        required : true 
+    }, 
+
+    totalTest : {
+        type : Number , 
+        default : 0 
+    }, 
+    score : {
+        type : Number , 
+        default : 0 
+    }, 
+    previous : [
+        {
+            type : String ,
+        }
+    ]
+
+}); 
+
+module.exports = mongoose.model("User" , UserSchema) ; 
